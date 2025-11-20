61b2210d3196be72f1085269bacbc5a1ceb0f26e
Upasanabasak <basakupasana2005@gmail.com>
2025-11-20 19:00:59 +0530

    Practice

--- Practical_Question_2024/Set_3/Q3.c
+++ Practical_Question_2024/Set_3/Q3.c
@@ -1,3 +1,97 @@
 /* Write a program in C to convert any number into its Roman Equivalent with following notation : 
 [1 --> I, 2 --> II, 5 --> V, 50 --> L, 100 --> C, 500 --> D, 1000 --> M] 
 */
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("\nEnter number : ");
+    scanf("%d", &num);
+    printf("\nRoman of %d is : ",num);
+    while(num > 0)
+    {
+        if(num > 1000)
+        {
+            printf("M");
+            num -= 1000;
+        }
+        else if(num >= 500)
+        {
+            if(num >= 900)
+            {
+                printf("CM");
+                num -= 900;
+            }
+            else
+            {
+                printf("D");
+                num -= 500;
+            }
+        }
+        else if(num >= 100)
+        {
+            if(num >= 400)
+            {
+                printf("CD");
+                num -= 400;
+            }
+            else
+            {
+                printf("C");
+                num -= 100;
+            }
+        }
+        else if(num >= 50)
+        {
+            if(num >= 90)
+            {
+                printf("XC");
+                num -= 90;
+            }
+            else
+            {
+                printf("L");
+                num -= 50;
+            }
+        }
+        else if(num >= 9)
+        {
+            if(num >= 40)
+            {
+                printf("XL");
+                num -= 40;
+            }
+            else if(num == 9)
+            {
+                printf("IX");
+                num -= 9;
+            }
+            else
+            {
+                printf("X");
+                num -= 10;
+            }
+        }
+        else if(num >= 4)
+        {
+            if(num >= 5)
+            {
+                printf("V");
+                num -= 5;
+            }
+            else
+            {
+                printf("IV");
+                num -= 4;
+            }
+        }
+        else
+        {
+            printf("I");
+            num -= 1;
+        }
+    }
+    return 0;
+}
--- Practical_Question_2024/Set_3/Q4.c
+++ Practical_Question_2024/Set_3/Q4.c
@@ -1,5 +1,90 @@
-/* Write a program in C to 
-   (a) copy one string into another without string function. 
+/* Write a program in C to
+   (a) copy one string into another without string function.
    (b) compare two strings without string function.
-
 */
+
+#include <stdio.h>
+
+int main()
+{
+   char a[20], b[20], c[20], option;
+   int i, j, flag, choice;
+
+   do
+   {
+      printf("\n1. String Copy");
+      printf("\n2. String Compare");
+      printf("\nEnter your choice : ");
+      scanf("%d", &choice);
+      getchar(); // to clear newline after scanf
+
+      switch (choice)
+      {
+         case 1:
+                  printf("\nEnter source string : ");
+                  fgets(a, sizeof(a), stdin);
+
+                  i = 0;
+                  j = 0;
+
+                  while (a[i] != '\0')
+                  {
+                     b[j] = a[i];
+                     i++;
+                     j++;
+                  }
+                  b[j] = '\0';
+
+                  printf("\nTarget string is : %s", b);
+                  break;
+
+         case 2:
+                  printf("\nEnter source string : ");
+                  fgets(a, sizeof(a), stdin);
+
+                  printf("\nEnter target string : ");
+                  fgets(c, sizeof(c), stdin);
+
+                  i = 0;
+                  j = 0;
+                  flag = 0;
+
+                  while (1)
+                  {
+                     if (a[i] == '\0' || c[j] == '\0')
+                     {
+                        if (a[i] != c[j])
+                        {
+                           flag = 1;
+                        }
+                        break;
+                     }
+
+                     if (a[i] != c[j])
+                     {
+                        flag = 1;
+                        break;
+                     }
+
+                     i++;
+                     j++;
+                  }
+
+                  if (flag == 0)
+                     printf("\nStrings are equal.");
+                  else
+                     printf("\nStrings are not equal.");
+
+                  break;
+
+         default:
+                  printf("\nWrong Choice.");
+      }
+
+         printf("\nDo you want to continue(Y/N)? : ");
+         scanf(" %c", &option); // space to ignore newline
+
+   } while (option == 'y' || option == 'Y');
+
+   return 0;
+}
